b308aed94f9f77252f0951b489b67a6abd4b843d
marlon <falconsoft.3d@gmail.com>
2019-09-10 20:17:21 -0300

    Working Footer

--- apps/webodoobim/views/views.py
+++ apps/webodoobim/views/views.py
@@ -10,3 +10,15 @@ from django.views.generic import DetailView, ListView
 
 def index(request):
     return render(request, 'webodoobim/index.html')
+
+def about(request):
+    return render(request, 'webodoobim/about.html')
+
+def services(request):
+    return render(request, 'webodoobim/services.html')
+
+def contact(request):
+    return render(request, 'webodoobim/contact_us.html')
+
+def blog(request):
+    return render(request, 'webodoobim/blog.html')
