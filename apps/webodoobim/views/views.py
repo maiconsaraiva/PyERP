78bdd1972173659f3ea823e986317bb2b72ea010
marlon <falconsoft.3d@gmail.com>
2019-09-10 20:30:42 -0300

    Working Footer

--- apps/webodoobim/views/views.py
+++ apps/webodoobim/views/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import HttpResponse, render
 from django.template.loader import render_to_string
 from django.views.generic import DetailView, ListView
 
+BIM_PHONE = "+56 9 4299 4534"
 
 def index(request):
     return render(request, 'webodoobim/index.html')
