1737352a6387ce802aef02ccd6ebf51ded9a5239
marlon <falconsoft.3d@gmail.com>
2019-09-15 02:16:59 -0300

    Iniciando con Paypal

--- apps/paypal/urls.py
+++ apps/paypal/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from .views.paypal_config import UpdatePaypalConfigView
 
+
+# http://www.secnot.com/django-shop-paypal-rest-1.html
+
 app_name = 'paypal'
 
 urlpatterns = [
