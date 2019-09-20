9df77e3a3e2591c4bc363538a9718eafa217809a
marlon <falconsoft.3d@gmail.com>
2019-09-19 22:07:35 -0300

    SC

--- apps/base/views/web_father.py
+++ apps/base/views/web_father.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from ..models import PyWParameter
+from ..models import PyWParameter, PyMeta
 
 def _web_parameter():
     web_parameter = {}
@@ -7,10 +7,18 @@ def _web_parameter():
         web_parameter[parametro.name] = parametro.value
     return web_parameter
 
+def _web_meta():
+    cad = ''
+    for meta in PyMeta.objects.all():
+        cad += '<meta name="'+meta.title+'" content="'+meta.content+'">' + '\n'
+    return cad
+
+
 class FatherListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['web_parameter'] = _web_parameter()
+        context['meta'] = _web_meta()
         return context
 
     class Meta:
@@ -20,6 +28,7 @@ class FatherDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['web_parameter'] = _web_parameter()
+        context['meta'] = _web_meta()
         return context
 
     class Meta:
