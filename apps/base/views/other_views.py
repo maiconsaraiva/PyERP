113bc472e406bd95d5114e30cec77be3646a8a6f
marlon <falconsoft.3d@gmail.com>
2019-09-20 19:35:45 -0300

    123

--- apps/base/views/other_views.py
+++ apps/base/views/other_views.py
@@ -18,8 +18,6 @@ from ..models.meta import PyMeta
 
 
 def IndexEasy(request):
-    PyMeta.LoadData()
-
     if BaseConfig.objects.all().exists():
         template = 'base/index.html'
     else:
@@ -40,7 +38,7 @@ def IndexEasy(request):
             # PyUser.crear(user, password, 1, 1, 1)
 
             """Leer las datas """
-            PyMeta.LoadData("base", PyMeta)
+            PyMeta.LoadData()
 
             user = PyUser.objects.create_user(user_name, None, password)
             user.is_staff = True
